1b602658e3c00c5b51e2a1f0935672de4f1ebc9c
PatrickQuinty <patrick.quinty1@gmail.com>
2019-12-02 16:51:37 -0500

    Created Working JSON parser

--- app/src/main/java/com/example/cst2335finalproject/FindCarCharger.java
+++ app/src/main/java/com/example/cst2335finalproject/FindCarCharger.java
@@ -5,10 +5,12 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -28,8 +30,11 @@ import java.util.ArrayList;
 public class FindCarCharger extends AppCompatActivity {
 
     ArrayList<ChargingStation> stations = new ArrayList<>();
+    ListView stationView;
+    BaseAdapter stationAdapter;
     CarDBOpenHelper dbOpener = new CarDBOpenHelper(this);
     SQLiteDatabase db;
+    String searchLatitude, searchLongitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,20 +45,23 @@ public class FindCarCharger extends AppCompatActivity {
         //db = dbOpener.getWritableDatabase();
 
 
-        ListView stationView = findViewById(R.id.stationList);
+        stationView = findViewById(R.id.stationList);
         Button search = findViewById(R.id.statnSrch);
         EditText latitudeIn = findViewById(R.id.latInput),
         longitudeIn = findViewById(R.id.longInput);
-        String latitude = latitudeIn.getText().toString(),
-        longitude = longitudeIn.getText().toString();
+        searchLatitude = latitudeIn.getText().toString();
+        searchLongitude = longitudeIn.getText().toString();
+
+
 
         if(search != null)
         {
             search.setOnClickListener(
                     v -> {
                         //TODO add code to launch search
-                        stationView.setVisibility(View.VISIBLE);
                         new ChargingStationQuery().execute();
+
+
                     }
             );
         }
@@ -68,8 +76,8 @@ public class FindCarCharger extends AppCompatActivity {
         }
 
         @Override
-        public Object getItem(int i) {
-            return stations.get(i); //TODO might need updating
+        public String[] getItem(int i) {
+            return stations.get(i).getStationInfo(); //TODO might need updating
         }
 
         @Override
@@ -79,16 +87,28 @@ public class FindCarCharger extends AppCompatActivity {
 
         @Override
         public View getView(int i, View view, ViewGroup viewGroup) {
-            View thisRow = null; //TODO remove null statement
+            View thisRow; //TODO remove null statement
+            thisRow = getLayoutInflater().inflate(R.layout.charge_station_row, null);
+            TextView title = thisRow.findViewById(R.id.stationLocationTitle),
+                    latitude = thisRow.findViewById(R.id.chargingLatitude),
+                    longitude = thisRow.findViewById(R.id.chargingLongitude),
+                    phone = thisRow.findViewById(R.id.chargeStationPhone);
+            String[] data = getItem(i);
+            title.setText(data[1]);
+            latitude.setText(data[2]);
+            longitude.setText(data[3]);
+            phone.setText(data[4]);
 
-                //TODO make a layout for a single station to show
+
+            //TODO make a layout for a single station to show
                 //thisRow = getLayoutInflater().inflate(R.layout.lab4_msg_receive_relative, null);
 
             return thisRow;
         }
     }
-    private class ChargingStationQuery extends AsyncTask<String, Integer, String[]> {
+    private class ChargingStationQuery extends AsyncTask<String, Integer, String> {
 
+        private String ret;
         private String[] id = new String[10],
                 title = new String[10],
                 latitude = new String[10],
@@ -96,13 +116,14 @@ public class FindCarCharger extends AppCompatActivity {
                 phone = new String[10];
 
         @Override
-        protected String[] doInBackground(String... strings) {
+        protected String doInBackground(String... strings) {
 
 
             try {
                 String chargeQuery = "https://api.openchargemap.io/v3/poi/?output=json&latitude=" +
-                        strings[0] + "&longitude=" + //should be the latitude
-                        strings[1] + "&maxresults=10"; //should be the longitude
+                        searchLatitude + "&longitude=" + //should be the latitude
+                        searchLongitude + "&maxresults=10"; //should be the longitude
+                chargeQuery = "https://api.openchargemap.io/v3/poi/?output=json&latitude=45.347571&longitude=-75.756140&maxresults=10";
                 URL chargingURL = new URL(chargeQuery);
                 HttpURLConnection chargingConnect = (HttpURLConnection)chargingURL.openConnection();
                 InputStream chargeInpStream = chargingConnect.getInputStream();
@@ -120,44 +141,59 @@ public class FindCarCharger extends AppCompatActivity {
 
                 JSONArray jsonArr = new JSONArray(result);
 
-                for (int i = 0; i<10; i++)
+                for (int i = 0; i<jsonArr.length(); i++)
                 {
                     JSONObject jsonObj = jsonArr.getJSONObject(i);
                     JSONObject address = jsonObj.getJSONObject("AddressInfo");
+                    id[i] = String.valueOf(jsonObj.getInt("ID"));
                     title[i] = address.getString("Title");
                     latitude[i] = String.valueOf(address.getDouble("Latitude"));
                     longitude[i] = String.valueOf(address.getDouble("Longitude"));
-                    phone[i] = jsonObj.getString("ContactTelephone1");
+                    if(!address.isNull("ContactTelephone1"))
+                    phone[i] = address.getString("ContactTelephone1");
+                    else
+                        phone[i] = "No phone number available";
                 }
 
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (JSONException e) {
+            }
+            catch(MalformedURLException mfe){ ret = "Malformed URL exception"; return ret;}
+            catch(IOException ioe)          { ret = "IO Exception. Is the Wifi connected?"; return ret;}
+            catch (JSONException e) {
                 e.printStackTrace();
             }
-            return new String[0];
+            return ret;
         }
         //TODO fill in the class
 
 
         @Override
-        protected void onPostExecute(String[] strings) {
-            super.onPostExecute(strings);
-            for (int i = 0; i < 10; i++)
+        protected void onPostExecute(String fromDoInBackground) {
+            super.onPostExecute(fromDoInBackground);
+            for (int i = 0; i < id.length; i++)
             { //loops through array of search results and adds each of them to arrayList as a new station object
                 stations.add(new ChargingStation(id[i], title[i], latitude[i], longitude[i], phone[i]));
             }
+            stationView.setAdapter(stationAdapter = new StationAdapter());
+            stationView.setVisibility(View.VISIBLE);
         }
     }
 
     private class ChargingStation
     {
 
+        String id, title, latitude, longitude, phone;
         public ChargingStation(String id, String title, String latitude, String longitude, String phone)
         {
+            this.id = id;
+            this.title = title;
+            this.latitude = latitude;
+            this.longitude = longitude;
+            this.phone = phone;
+        }
 
+        public String[] getStationInfo()
+        {
+            return new String[]{id, title, latitude, longitude, phone};
         }
     }
 }
